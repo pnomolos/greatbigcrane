8ee45e1d11ec2c48f9532bdaf2609a5f2488125e
Dusty Phillips <dusty@archlinux.ca>
2010-08-14 03:46:30 -0600

    Add license to test files.

--- greatbigcrane/preferences/tests.py
+++ greatbigcrane/preferences/tests.py
@@ -1,23 +1,15 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+Copyright 2010 Jason Chu, Dusty Phillips, and Phil Schalm
 
-Replace these with more appropriate tests for your application.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
-
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
