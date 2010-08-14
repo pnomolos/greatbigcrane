c3d33be6ec030553693fcf681559051c65a6d3ef
Philip <pnomolos@gmail.com>
2010-08-14 10:39:58 -0700

    Some nice AJAX stuff.

--- greatbigcrane/media/js/script.js
+++ greatbigcrane/media/js/script.js
@@ -2,8 +2,19 @@ function slugify(string) {
     return string.replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
 }
 
+function ajaxRequest( url, _data ) {
+  callback = _data['update'] ? function(data) { $(_data['update']).html(data.html) } : $.noop;
+  $.post( url, data, callback );
+}
+
 jQuery(function($){
   $('.autosubmit').change(function(){
     $(this).parents('form').first().submit();
-  })
+  });
+  
+  $('.buttons .favourite').click(function(e){
+    e.preventDefault();
+    ajaxRequest( $(this).attr('href'), { update: '#project-list' } );
+  });
 });
+
