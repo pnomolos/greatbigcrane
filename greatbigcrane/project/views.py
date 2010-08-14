9b34aad2b17dadd50ea7836093cef6a11f1734af
Dusty Phillips <dusty@archlinux.ca>
2010-08-14 01:31:01 -0600

    Add a request context.

--- greatbigcrane/project/views.py
+++ greatbigcrane/project/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render_to_response
 from django.views.generic.list_detail import object_list
 from django.views.generic.list_detail import object_detail
 from django.views.generic.create_update import create_object
+from django.template import RequestContext
 from project.models import Project
 
 def index(request):
     '''We should move this to a different app'''
-    return render_to_response('index.html')
+    return render_to_response('index.html', RequestContext(request, {}))
 
 def list_projects(request):
     projects = Project.objects.all()
