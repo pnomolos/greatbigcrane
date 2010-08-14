1015d05cb9ef0bbfe1808eff5b53e70c57aba6ad
Jason Chu <jchu@xentac.net>
2010-08-14 11:04:40 -0700

    Add licenses to all files that didn't have them

--- greatbigcrane/notifications/tests.py
+++ greatbigcrane/notifications/tests.py
@@ -1,8 +1,17 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+Copyright 2010 Jason Chu, Dusty Phillips, and Phil Schalm
 
-Replace these with more appropriate tests for your application.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 from django.test import TestCase
