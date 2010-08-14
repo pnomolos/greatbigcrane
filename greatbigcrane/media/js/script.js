42496b324a8628a9016d9750a3c71aa3d02b4a5b
Philip <pnomolos@gmail.com>
2010-08-14 09:23:19 -0700

    Let's fancy this up with some js.

--- greatbigcrane/media/js/script.js
+++ greatbigcrane/media/js/script.js
@@ -1,3 +1,9 @@
 function slugify(string) {
     return string.replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
 }
+
+jQuery(function($){
+  $('.autosubmit').change(function(){
+    $(this).parents('form').first().submit();
+  })
+});
