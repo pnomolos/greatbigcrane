5ccec38532ead12694a76d00dbdb48d1ebb4c579
Dusty Phillips <dusty@archlinux.ca>
2010-08-14 15:43:55 -0600

    WTF@\!

--- greatbigcrane/notifications/models.py
+++ greatbigcrane/notifications/models.py
@@ -22,7 +22,7 @@ make_choice = lambda x: ([(p,p) for p in x])
 
 class Notification(models.Model):
     status = models.CharField(max_length=15, choices=make_choice([
-        "success", "error", "general!"]))
+        "success", "error", "general"]))
     summary = models.CharField(max_length=128)
     message = models.TextField()
     notification_time = models.DateTimeField(auto_now_add=True)
