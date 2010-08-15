ba42cf4b7df9e68c2cd8c0d3800b97fe42d77abb
Philip <pnomolos@gmail.com>
2010-08-14 23:42:07 -0700

    Cleaning up javascript.

--- greatbigcrane/media/js/script.js
+++ greatbigcrane/media/js/script.js
@@ -2,8 +2,8 @@ function slugify(string) {
   return string.replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
 }
 
-function ajaxHandler( data ) {
-  if ( data.update ) {
+function ajaxHandler(data) {
+  if (data.update) {
     $.each(data.update,function(k,v){
       $(k).html(v);
     });
@@ -14,15 +14,15 @@ function dismiss_notification(notification_id) {
   $.get("/notifications/dismiss/" + notification_id + "/", {},
     function(data, textStatus, xhr) {
       $("#notification-" + notification_id).slideUp(function(){$(this).remove()});
-    });
+  });
 }
 
 function load_recipe_template(project_id) {
   return (function(node) {
     if ($('#available_recipes').val() != "") {
       $("#recipe_template_container").load("/projects/" + project_id +
-          "/recipe_template/" +
-          $('#available_recipes').val() + '/');
+        "/recipe_template/" +
+        $('#available_recipes').val() + '/');
     }
   })
 }
@@ -44,7 +44,7 @@ function show_buildout_result(node) {
         break;
     }
     to_append = $(to_append).css('display','none');
-    $(to_append).appendTo(node.parents('li')).slideDown().delay(2000).slideUp(function(){$(this).remove();});
+    $(to_append).appendTo(node.parents('li')).slideDown().delay(2000).slideUp(function(){$(this).remove()});
   });
 }
 
@@ -56,14 +56,14 @@ jQuery(function($){
   $('div.projects .favourite').live('click', function(e){
     e.preventDefault();
     var href = $(this).attr('href') + '?' + $(this).parents('form').serialize();
-    $.post( href, { update: '{"#project-list":"projects"}' }, ajaxHandler );
+    $.post(href, {update: '{"#project-list":"projects"}'}, ajaxHandler);
   });
 
   $('div.dashboard .projects .favourite').live('click', function(e){
     e.preventDefault();
-    $.post( $(this).attr('href'), { 
+    $.post($(this).attr('href'), { 
       update: '{"#project-list":"home-projects","#favourite-project-list":"favourite-projects"}' 
-    }, ajaxHandler );
+    }, ajaxHandler);
   });
 
   $('div.dashboard .projects .buildout, div.dashboard .projects .tests').live('click', function(e){
@@ -85,28 +85,6 @@ jQuery(function($){
       $('div.dashboard ul.tests li:nth-child(11n)').slideUp(function(){$(this).remove()});
   });
   
-  // if ($('.notifications').size()) {
-  //   var paper = Raphael(
-  //     $(".notifications .svg").get(0),
-  //     100,
-  //     Math.max($('.successes').height(), $('.errors').height())
-  //   );
-  //   var $paper = $(".notifications .svg").first();
-  //   var first_success = parseInt($('.successes li').first().attr('id').replace(/\D*/,''));
-  //   var first_error = parseInt($('.errors li').first().attr('id').replace(/\D*/,''));
-  //   
-  //   var cur_id = Math.max(first_success, first_error);
-  //   do {
-  //     var node = $('#notification-' + cur_id);
-  //     var prev_node = $('#notification-' + (cur_id + 1));
-  //     if ( node.size() && prev_node.size() && node.attr('class') != prev_node.attr('class')) {
-  //       var line = paper.path("M{0},{1} L{2},{3}",
-  //         prev_node.hasClass('error')?100:0, prev_node.position().top - $paper.position().top + (prev_node.height()/2),
-  //         node.hasClass('error')?100:0, node.position().top - $paper.position().top + (node.height()/2)
-  //       ).attr('stroke', prev_node.children('a').css('background-color')).attr('stroke-width', 2);
-  //     }
-  //     cur_id--;
-  //   } while ( node.size() )
-  // }
+  $("#id_eggs, #id_extra_paths").lineeditor();
 });
 
