6976c4ac43ef6ac95bf6effdf04c496d6c7a654d
Philip <pnomolos@gmail.com>
2010-08-14 22:37:17 -0700

    Sweet dealio!

--- greatbigcrane/media/js/jquery.lineeditor.js
+++ greatbigcrane/media/js/jquery.lineeditor.js
@@ -6,7 +6,7 @@
       
       if ( el.nodeName.toLowerCase() != 'textarea' ) { return; }
       
-      var hidden = $('<input type="hidden"/>').attr('id', el.id);      
+      var hidden = $('<input type="hidden"/>').attr('id', el.id).attr('name',el.name);      
       var container = $('<div class="lineeditor"></div>');
       var val = $el.val();
       
@@ -22,11 +22,12 @@
       $el.replaceWith(container);
       
       function addLine(value) {
-        value = ( value.target ? value.preventDefault() && '' : value );
-        var node = $('<input type="text"/>').val(value?value.toString():'');
+        value = ( value && value.target ? value.preventDefault() && '' : value );
+        var node = $('<input type="text"/>').val(value?value.toString():'').keydown(processValues);
         var delete_node = $('<a href="#delete" class="delete">Delete</a>').click(removeLine);
         container.find('a:last').before(node, delete_node);
         processValues();
+        return node;
       }
       
       function removeLine(ev) {
@@ -36,12 +37,22 @@
         processValues();
       }
       
-      function processValues() {
-        var val = '';
-        container.find('input[type=text]').each(function(){
-          val += $(this).val() + "\n";
-        })
-        hidden.val(val);
+      function processValues(e) {
+        if (e && e.keyCode == 9) {
+          e.preventDefault();
+          if ( !e.shiftKey ) {
+            $(e.target).nextAll('input[type=text]:first').focus().size() || addLine().focus(); 
+          } else {
+            console.log($(e.target));
+            $(e.target).prevAll('input[type=text]:first').focus();
+          } 
+          return;
+        }
+        hidden.val(
+          container.find('input[type=text]').map(function(){
+            return $(this).val();
+          }).toArray().join("\n").trim()
+        );
       }
       
     })
