63503e4f6b9005f6426ca8f46bfc85a16b9e2123
Dusty Phillips <dusty@archlinux.ca>
2010-08-14 21:21:50 -0600

    slightly more sensible urls on recipes

--- greatbigcrane/project/urls.py
+++ greatbigcrane/project/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('project.views',
         url('^(?P<project_id>\d+)/buildout$', 'schedule_buildout', name="schedule_buildout"),
         url('^add/$', 'add_project', name="add_project"),
         url('^favourite/(?P<project_id>\d+)/$', 'favourite_project', name="favourite_project"),
-        url('^add_recipe/(?P<project_id>\d+)/$', 'add_recipe', name="add_recipe"),
-        url('^save_recipe/(?P<project_id>\d+)/$', 'save_recipe', name="save_recipe"),
+        url('^recipe/add/(?P<project_id>\d+)/$', 'add_recipe', name="add_recipe"),
+        url('^recipe/save/(?P<project_id>\d+)/$', 'save_recipe', name="save_recipe"),
         url('^recipe_template/(?P<project_id>\d+)/(?P<recipe_name>[^/]*)/$', 'recipe_template', name='recipe_template')
         )
