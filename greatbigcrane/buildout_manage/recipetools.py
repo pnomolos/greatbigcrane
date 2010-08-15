55667db805280490d819f5b4dda560f1910ffbea
Jason Chu <jchu@xentac.net>
2010-08-15 08:05:10 -0700

    Make recipe properties less breaky and get rid of the whole tuple expansion by default

--- greatbigcrane/buildout_manage/recipetools.py
+++ greatbigcrane/buildout_manage/recipetools.py
@@ -25,48 +25,45 @@ def add_parts(config, section_name):
 
 section_re = re.compile(r'\${([^:]*):([^}]*)}')
 
+def expand_value(value):
+    """
+    Converts ${buildout:directory}/something/something into
+    (('buildout', 'directory'), '/something/something)
+    """
+    tokens = section_re.split(value)
+    token_list = []
+    if tokens[0]:
+        token_list.append(tokens[0])
+    steps = (len(tokens) - 1)/3
+    for step in range(steps):
+        token_list.append(tuple(tokens[step*3+1:step*3+3]))
+        if tokens[step*3+3]:
+            token_list.append(tokens[step*3+3])
+    return tuple(token_list)
+
+def compress_value(value):
+    """
+    Converts (('buildout', 'directory'), '/something/something) into
+    ${buildout:directory}/something/something
+    """
+    data_list = []
+    for elem in value:
+        if isinstance(elem, tuple):
+            data_list.append('${%s:%s}' % elem)
+        else:
+            data_list.append(elem)
+    return ''.join(data_list)
+
 def simple_property_get(name):
     def get(self):
-        # Parse ${
-        def convert_value(data):
-            if '${' in data:
-                tokens = section_re.split(data)
-                token_list = []
-                if tokens[0]:
-                    token_list.append(tokens[0])
-                steps = (len(tokens) - 1)/3
-                for step in range(steps):
-                    token_list.append(tuple(tokens[step*3+1:step*3+3]))
-                    if tokens[step*3+3]:
-                        token_list.append(tokens[step*3+3])
-                return tuple(token_list)
-            return data
-
+        if name not in self.section:
+            return ''
         value = self.section[name]
-        if isinstance(value, list):
-            value = [convert_value(x) for x in value]
-        else:
-            value = convert_value(value)
         return value
     return get
 
 def simple_property_set(name):
     def set(self, value):
-        def convert_value(data):
-            if isinstance(data, tuple):
-                data_list = []
-                for elem in data:
-                    if isinstance(elem, tuple):
-                        data_list.append('${%s:%s}' % elem)
-                    else:
-                        data_list.append(elem)
-                data = ''.join(data_list)
-            return data
-
-        if isinstance(value, list):
-            value = [convert_value(x) for x in value]
-        else:
-            value = convert_value(value)
         self.section[name] = value
     return set
 
@@ -79,6 +76,8 @@ simple_property = lambda name: property(simple_property_get(name), simple_proper
 
 def bool_property_get(name):
     def get(self):
+        if name not in self.section:
+            return ''
         value = self.section[name]
         if value.lower() == 'true':
             return True
